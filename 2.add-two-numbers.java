ffa2749c25788ffa2c384e9ae45906e6f66be9ae
Yuwei Xie <yuwei@yuweis-mbp.mynetworksettings.com>
2023-08-22 20:33:41 -0400

    leetcode 3

---
+++ 2.add-two-numbers.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode p1 = l1, p2 = l2;
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (p1 != null || p2 != null) {
+            int val = carry;
+            if (p1 != null) {
+                val += p1.val;
+                p1 = p1.next;
+            }
+
+            if (p2 != null) {
+                val += p2.val;
+                p2 = p2.next;
+            }
+
+            carry = val / 10;
+            val = val % 10;
+            curr.next = new ListNode(val);
+            curr = curr.next;
+        }
+
+        if (carry == 1) {
+            curr.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
+
---
+++ 3.longest-substring-without-repeating-characters.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=3 lang=java
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> window = new HashMap<>();
+        int left = 0, right = 0;
+        int res = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            while (window.get(c) > 1) {
+                window.put(s.charAt(left), window.get(s.charAt(left)) - 1);
+                left++;
+            }
+
+            res = Math.max(res, right - left);
+        }
+
+        return res;
+        
+    }
+}
+// @lc code=end
+
